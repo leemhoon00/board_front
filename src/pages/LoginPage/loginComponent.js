b465a05ecc9d6c11bfd586d19f24a6f0596fd4c0
leemhoon00 <leemhoon00@naver.com>
2023-02-28 19:19:51 +0900

    design: align center

--- src/pages/LoginPage/loginComponent.js
+++ src/pages/LoginPage/loginComponent.js
@@ -1,10 +1,26 @@
 import * as React from "react";
 
 class LoginComponent extends React.Component{
+  submitEvent = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        dddd
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+      }}>
+        <form className="row" style={{
+          
+          
+        }}>
+          <input name="id" class="col-12" placeholder="아이디" required type="text"/>
+          <input name="pw" class="col-12" placeholder="비밀번호" required type="password" />
+        </form>
       </div>
     );
   }
