4b93c44c75b918ff55c8c29424baf7b64fffbd47
leemhoon00 <leemhoon00@naver.com>
2023-03-02 19:38:16 +0900

    feat: logout router, some test code

--- src/pages/BoardsPage/BoardsComponent.js
+++ src/pages/BoardsPage/BoardsComponent.js
@@ -1,16 +1,32 @@
 import axios from 'axios';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../store';
+import { useNavigate } from 'react-router-dom';
 
 function BoardsComponent() {
   const user = useRecoilValue(userState);
+
+  const navigate = useNavigate();
+
+  const clickEvent = (event) => {
+    axios.post('http://localhost:3001/auth/test',{},{withCredentials: true});
+  }
+
+  const LogoutEvent = (event) => {
+    axios.get('http://localhost:3001/auth/test', {withCredentials: true})
+      .then((res) => {
+        navigate('/');
+      })
+  }
   return(
     <div style={{
       display: 'flex',
       justifyContent: 'center',
     }}>
       <h1>게시판</h1>
-      <label>{user}</label>
+      <label>{user}</label><br></br>
+      <button onClick={clickEvent}>OK</button>
+      <button onClick={LogoutEvent}>LOGOUT</button>
     </div>
   );
 }
